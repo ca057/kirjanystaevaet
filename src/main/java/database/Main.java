bec43c5ecc22f23ad6e0116846cfe60e0e3328d3
HannesT117 <j.trepesch@outlook.de>
2015-11-11 17:34:14 +0100

    Fixed Bug in database/Main

--- src/main/java/database/Main.java
+++ src/main/java/database/Main.java
@@ -4,10 +4,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import conf.RootConfig;
+
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringRootConfig.class);
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(RootConfig.class);
 		JdbcTemplate jdbc = ctx.getBean(JdbcTemplate.class);
 		
 		System.out.println(jdbc.queryForObject("select count(*) from nameF", Integer.class));
