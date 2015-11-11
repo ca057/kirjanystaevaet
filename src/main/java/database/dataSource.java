ff2b7b802d344cc499fd96db93b239ce608df233
HannesT117 <j.trepesch@outlook.de>
2015-11-11 16:59:10 +0100

    Added some stuff for database impl

---
+++ src/main/java/database/dataSource.java
@@ -0,0 +1,21 @@
+package database;
+	import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+	
+	public class dataSource {
+		@Bean
+		public DataSource dataSource() {
+			
+			// no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
+			EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+			EmbeddedDatabase db = builder
+				.setType(EmbeddedDatabaseType.H2) //.H2 or .DERBY
+				.addScript("Webshop_Material/DataDump_h2.txt")
+				.build();
+			return db;
+		}
+}
