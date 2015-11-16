66dbd889effe7980cfce256be8975ef45fcb35b8
HannesT117 <j.trepesch@outlook.de>
2015-11-16 15:54:27 +0100

    Added some test code. Database is persistent now.

--- src/main/java/conf/RootConfig.java
+++ src/main/java/conf/RootConfig.java
@@ -1,5 +1,11 @@
 package conf;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
@@ -11,28 +17,40 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
 public class RootConfig {
-	
+
 	@Bean
 	public DataSource getDataSource() {
 		// no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
 		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		EmbeddedDatabase db = builder
-			.setType(EmbeddedDatabaseType.H2)
-			// TODO File auswechseln
-			.addScript("classpath:resources/DataDump_h2.txt")
-			.build();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2).addScript("classpath:resources/DataDump_h2.txt")
+				.build();
+
+		try {
+			Class.forName("org.h2.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String url = "jdbc:h2:" + "./testdb.db";
+		String user = "aeter";
+		String password = "aeter";
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			PreparedStatement ps2 = conn.prepareStatement("Show tables;");
+			ResultSet rs = ps2.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return db;
 	}
 
 	@Bean
 	public JdbcTemplate getJdbcTemplate() {
-//		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-//		EmbeddedDatabase db = builder
-//			.setType(EmbeddedDatabaseType.H2)
-//			// TODO File auswechseln
-//			.addScript("classpath:resources/DataDump_h2.txt")
-//			.build();
-		return new JdbcTemplate(getDataSource());
+		return new JdbcTemplate(new DataSourceInitializer().getDataSource());
 	}
 
 }
