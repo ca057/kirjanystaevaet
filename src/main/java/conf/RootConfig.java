0bbb7c715d706675f54a4ca360f4e40a6abe67cf
HannesT117 <j.trepesch@outlook.de>
2015-11-16 14:42:15 +0100

    Commented ShopInit and fixed RootConfig

--- src/main/java/conf/RootConfig.java
+++ src/main/java/conf/RootConfig.java
@@ -3,34 +3,36 @@ package conf;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
+@Configuration
 public class RootConfig {
 	
-//	@Bean
-//	public DataSource getDataSource() {
-//		// no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
-//		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-//		EmbeddedDatabase db = builder
-//			.setType(EmbeddedDatabaseType.H2)
-//			// TODO File auswechseln
-//			.addScript("classpath:resources/DataDump_h2.txt")
-//			.build();
-//		return db;
-//	}
-
 	@Bean
-	public JdbcTemplate getJdbcTemplate() {
+	public DataSource getDataSource() {
+		// no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
 		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
 		EmbeddedDatabase db = builder
 			.setType(EmbeddedDatabaseType.H2)
 			// TODO File auswechseln
 			.addScript("classpath:resources/DataDump_h2.txt")
 			.build();
-		return new JdbcTemplate(db);
+		return db;
+	}
+
+	@Bean
+	public JdbcTemplate getJdbcTemplate() {
+//		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+//		EmbeddedDatabase db = builder
+//			.setType(EmbeddedDatabaseType.H2)
+//			// TODO File auswechseln
+//			.addScript("classpath:resources/DataDump_h2.txt")
+//			.build();
+		return new JdbcTemplate(getDataSource());
 	}
 
 }
