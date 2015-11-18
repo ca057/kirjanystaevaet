c5a8174445074bc3de265ecf4b33437dc8cf4a31
HannesT117 <j.trepesch@outlook.de>
2015-11-18 17:38:52 +0100

    Added some methods to DataQuery

--- src/main/java/database/BookDataQuery.java
+++ src/main/java/database/BookDataQuery.java
@@ -1,5 +1,15 @@
 package database;
 
+import java.util.Map;
+
 public interface BookDataQuery {
+
+	public Map searchBooks(Map<Enum<Searchfields>, String> searchterms, Enum<SearchOptions> searchOptions);
+
+	public Map searchAuthors(Map<Enum<Searchfields>, String> searchterms, Enum<SearchOptions> searchOptions);
+
+	public Map searchCategories(Map<Enum<Searchfields>, String> searchterms, Enum<SearchOptions> searchOptions);
+
+	public Map searchAll(Map<Enum<Searchfields>, String> searchterms, Enum<SearchOptions> searchOptions);
 
 }
