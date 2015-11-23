e15dfc4bc04da47c045f1bf5d879133c4a9d6386
HannesT117 <j.trepesch@outlook.de>
2015-11-23 15:35:53 +0100

    Added Enums

---
+++ src/main/java/database/SearchOptions.java
@@ -0,0 +1,5 @@
+package database;
+
+public enum SearchOptions {
+	AND, OR;
+}
---
+++ src/main/java/database/Searchfields.java
@@ -0,0 +1,5 @@
+package database;
+
+public enum Searchfields {
+	AUTHORID, NAMEF, NAMEL, ISBN, SEARCHOR, SEARCHAND;
+}
