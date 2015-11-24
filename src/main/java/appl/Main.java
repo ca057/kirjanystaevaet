320a7a980bfe22b6d3983fb6c91049afa1b6e0a8
HannesT117 <j.trepesch@outlook.de>
2015-11-24 12:52:04 +0100

    Refactored structure

--- src/main/java/appl/Main.java
+++ src/main/java/appl/Main.java
@@ -1,5 +1,25 @@
 package appl;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import conf.RootConfig;
+
 public class Main {
 
+	public static void main(String[] args) {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(RootConfig.class);
+		JdbcTemplate jdbc = ctx.getBean(JdbcTemplate.class);
+
+		System.out.println(jdbc.queryForObject("select count(*) FROM PUBLIC.bookauthors", Integer.class));
+		// jdbc.execute("INSERT INTO PUBLIC.bookauthors (`AuthorID`, `nameF`,
+		// `nameL`) VALUES (1, 'Jason', 'Gilmore');"); -> Funktioniert
+		// (Fehlermeldung kommt)
+		jdbc.execute("DELETE FROM PUBLIC.bookauthors WHERE `nameF`='Jason' AND `nameL`='Gilmore';");
+		jdbc.execute("DELETE FROM PUBLIC.bookauthors WHERE `nameF`='Tschäisen' AND `nameL`='Gilmore';");
+		jdbc.execute("INSERT INTO PUBLIC.bookauthors (`nameF`, `nameL`) VALUES ('Jason', 'Gilmore');");
+		jdbc.execute("INSERT INTO PUBLIC.bookauthors (`nameF`, `nameL`) VALUES ('Tschäisen', 'Gilmore');");
+		System.out.println(jdbc.queryForObject("select count(*) FROM PUBLIC.bookauthors", Integer.class));
+	}
 }
