c1d7a576771fee56069803cbfd4caaad3d27dfc1
Christian Ost <christian@christianost.de>
2015-11-25 17:26:17 +0100

    Added useful values to the dummy list of categories

--- src/main/java/appl/queryManagement/QueryCreatorImpl.java
+++ src/main/java/appl/queryManagement/QueryCreatorImpl.java
@@ -2,30 +2,23 @@ package appl.queryManagement;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-public class QueryCreatorImpl implements QueryCreator{
-/*
-	@Override
-	public Map getBookByID(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-*/
+public class QueryCreatorImpl implements QueryCreator {
+	/*
+	 * @Override public Map getBookByID(String id) { // TODO Auto-generated
+	 * method stub return null; }
+	 */
 	@Override
 	public List<String> getCategories() {
 		// TODO vernünftige Abfrage an die Datenbank
 		List<String> categoryList = new ArrayList<String>();
 
-		categoryList.add("Bla");
-		categoryList.add("blub");
-		categoryList.add("bli");
-		categoryList.add("Fisch");
-		
-		
+		categoryList.add("Java");
+		categoryList.add("PHP");
+		categoryList.add("MySQL");
+		categoryList.add("mysql");
+
 		return categoryList;
 	}
-	
-	
 
 }
