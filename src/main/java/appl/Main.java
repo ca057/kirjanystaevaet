e9e639003694ff19131a7a827c2aca382ec08242
HannesT117 <j.trepesch@outlook.de>
2015-11-25 17:27:17 +0100

    Added BookRowMapper, Book and some query in Main

--- src/main/java/appl/Main.java
+++ src/main/java/appl/Main.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import appl.databasemanagement.databaseImpl.BookRowMapper;
+import apps.items.Book;
 import conf.RootConfig;
 
 public class Main {
@@ -26,10 +29,10 @@ public class Main {
 		// VALUES ('Jason', 'Gilmore');");
 		// jdbc.execute("INSERT INTO PUBLIC.bookauthors (`nameF`, `nameL`)
 		// VALUES ('Tschäisen', 'Gilmore');");
-
-		List<String> map = jdbc.queryForList("SELECT * FROM PUBLIC.bookauthors", String.class);
-		for (String o : map) {
-			System.out.println(o);
+		RowMapper rowMapper = new BookRowMapper();
+		List<Book> list = jdbc.query("SELECT * FROM PUBLIC.bookauthors", rowMapper);
+		for (Book o : list) {
+			System.out.println(o.getTitle());
 		}
 		// System.out.println(jdbc.queryForObject("select count(*) FROM
 		// PUBLIC.bookauthors", Integer.class));
