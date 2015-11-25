3f7b35f79f3bbc6b20a7468fa66f478747cc2504
Christian Ost <christian@christianost.de>
2015-11-25 17:22:58 +0100

    Added exception, category gets checked when requested

--- src/main/java/web/CategoriesController.java
+++ src/main/java/web/CategoriesController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import appl.queryManagement.queryCreator;
+import appl.queryManagement.queryCreatorImpl;
+import exceptions.CategoryNotFoundException;
+
 /**
  * Controller for the route to the categories.
  */
@@ -13,8 +17,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CategoriesController {
 
 	@RequestMapping(value = "/kategorie/{category}", method = RequestMethod.GET)
-	public String categories(@PathVariable("category") String category, Model m) {
-		m.addAttribute("name", category);
-		return "categories";
+	public String categories(@PathVariable("category") String category, Model m) throws CategoryNotFoundException {
+		if (checkIfExistingCategory(category)) {
+			m.addAttribute("name", category);
+			return "categories";
+		} else {
+			throw new CategoryNotFoundException("Die Kategorie " + category + " wurde nicht gefunden.");
+		}
+	}
+
+	private boolean checkIfExistingCategory(String category) {
+		queryCreator query = new queryCreatorImpl();
+		return query.getCategories().contains(category);
 	}
 }
