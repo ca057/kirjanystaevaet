cb02dc5ca8352efc3703866738fd9b742e881bd3
Ludwig <l.thormann@gmx.de>
2015-11-25 18:07:48 +0100

    QueryController aktuaalisiert

---
+++ src/main/java/web/QueryController.java
@@ -0,0 +1,24 @@
+package web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class QueryController {
+
+	@RequestMapping(value = "/suche", method = RequestMethod.GET)
+	public String query(
+			@RequestParam(value="title") String title,
+			@RequestParam(value="author") String author,
+			@RequestParam(value="isbn") String isbn,
+			@RequestParam(value="year") String year,
+			@RequestParam(value="category") String category,
+			Model m
+			) {
+				return "search";
+			}
+}
