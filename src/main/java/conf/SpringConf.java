f8d49966de690762a90199d659562b970c362725
Madeleine <madeleine@rosenhagen.de>
2015-11-25 17:40:18 +0100

    Added some more Bean

--- src/main/java/conf/SpringConf.java
+++ src/main/java/conf/SpringConf.java
@@ -3,14 +3,37 @@ package conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import appl.databasemanagement.BookDataAdmin;
+import appl.databasemanagement.BookDataQuery;
+import appl.databasemanagement.databaseImpl.BookDataAdminImpl;
+import appl.databasemanagement.databaseImpl.BookDataQueryImpl;
+import appl.ordermanagement.Order;
+import appl.ordermanagement.ordermanagementImpl.OrderImpl;
 import appl.queryManagement.QueryCreator;
 import appl.queryManagement.QueryCreatorImpl;
 
 @Configuration
 public class SpringConf {
+	// Beans for Controller
 	@Bean
 	public QueryCreator queryCreatorBean(){
 		return new QueryCreatorImpl();
 	}
 
+	/*
+	@Bean
+	public Order orderBean(){
+		return new OrderImpl();
+	}
+	*/
+	// Beans for Model
+	@Bean
+	public BookDataAdmin bookDataAdminBean(){
+		return new BookDataAdminImpl();
+		
+	}
+	@Bean
+	public BookDataQuery bookDataQueryBean(){
+		return new BookDataQueryImpl();
+	}
 }
