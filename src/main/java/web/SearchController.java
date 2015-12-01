83632bdd96f154b4e4903d87eebd553531e89dd7
Ludwig <l.thormann@gmx.de>
2015-12-01 14:00:50 +0100

    SearchController erweitert

--- src/main/java/web/SearchController.java
+++ src/main/java/web/SearchController.java
@@ -1,13 +1,27 @@
 package web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import appl.items.Book;
+import appl.queryManagement.QueryCreatorImpl;
+
 @Controller
 public class SearchController {
+
+	@Autowired
+	private QueryCreatorImpl query;
+
+	public void setQuery(QueryCreatorImpl query) {
+		this.query = query;
+	}
 
 	@RequestMapping(value = "/suche", method = RequestMethod.GET)
 	public String query(@RequestParam(value = "all", required = false) String all,
@@ -16,22 +30,36 @@ public class SearchController {
 			@RequestParam(value = "isbn", required = false) String isbn,
 			@RequestParam(value = "year", required = false) String year,
 			@RequestParam(value = "category", required = false) String category, Model m) {
-		String query = "Keine Suchanfrage gestellt.";
+		String queryTerm = "Keine Suchanfrage gestellt.";
+		List<Book> results = new ArrayList();
 		if (all != null && !all.isEmpty()) {
-			query = all;
+			results = query.getBooksByOpenSearch(all);
+			queryTerm = all;
 		} else {
+			String searchTitle = "";
+			String searchAuthor = "";
+			String searchYear = "";
+			String searchIsbn = "";
+			String searchCategory = "";
 			if (title != null && !title.isEmpty()) {
+				searchTitle = title;
 			}
-			if (author != null && !title.isEmpty()) {
+			if (author != null && !author.isEmpty()) {
+				searchAuthor = author;
 			}
 			if (isbn != null && !isbn.isEmpty()) {
+				searchIsbn = isbn;
 			}
 			if (year != null && !year.isEmpty()) {
+				searchYear = year;
 			}
 			if (category != null && !category.isEmpty()) {
+				searchCategory = category;
 			}
+			results = query.getBooksByMetadata(searchTitle, searchAuthor, searchYear, searchIsbn, searchCategory);
 		}
-		m.addAttribute("query", query);
+		m.addAttribute("query", queryTerm);
+		m.addAllAttributes(results);
 		return "search";
 	}
 }
