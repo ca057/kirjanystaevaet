6f8d314f26a3efaa0c8a4809ea3c188ab4abbc9d
Christian Ost <christian@christianost.de>
2015-12-01 13:20:52 +0100

    Updated search controller and jsp

--- WebContent/WEB-INF/views/search.jsp
+++ WebContent/WEB-INF/views/search.jsp
@@ -2,19 +2,20 @@
 <%@ page session="false" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 
 <section>
-	<p>Hier ist die Suche</p>
+	<h2>Suche</h2>
 	<form>
-		Titel: <br>
-		<input type="text" name="title">
-		Autor_In: <br>
-		<input type="text" name="author">
-		ISBN: <br>
-		<input type="text" name="isbn">
-		Erscheinungsjahr: <br>
-		<input type="text" name="year">
-		Kategorie: <br>
-		<input type="text" name="category">
-		<br>
+		<label for="title">Titel</label>
+		<input type="text" id="title" name="title"><br>
+		<label for="title">AutorIn</label>
+		<input type="text" name="author"><br>
+		<lable for="isbn">ISBN</lable>
+		<input type="text" name="isbn"><br>
+		<label for="year">Erscheinungsjahr</label>
+		<input type="text" name="year"><br>
+		<lable for="category">Kategorie</label>
+		<input type="text" name="category"><br>
 		<input type="submit" value="Los gehts :)">
 	</form>
+	
+	<h3>Suchergebnisse</h3>
 </section>
--- src/main/java/web/SearchController.java
+++ src/main/java/web/SearchController.java
@@ -7,18 +7,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-
 public class SearchController {
 
 	@RequestMapping(value = "/suche", method = RequestMethod.GET)
-	public String query(
-			@RequestParam(value="title") String title,
-			@RequestParam(value="author") String author,
-			@RequestParam(value="isbn") String isbn,
-			@RequestParam(value="year") String year,
-			@RequestParam(value="category") String category,
-			Model m
-			) {
-				return "search";
-			}
+	public String query(@RequestParam(value = "all", required = false) String all,
+			@RequestParam(value = "title", required = false) String title,
+			@RequestParam(value = "author", required = false) String author,
+			@RequestParam(value = "isbn", required = false) String isbn,
+			@RequestParam(value = "year", required = false) String year,
+			@RequestParam(value = "category", required = false) String category, Model m) {
+		if (all != null && !all.isEmpty()) {
+			System.out.println("Offene Suche über: " + all);
+		} else {
+
+		}
+		return "search";
+	}
 }
