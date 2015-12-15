0ab5107bd394b208359a51ebb2acb5c34eff1158
Christian Ost <christian@christianost.de>
2015-12-15 15:40:59 +0100

    Lösung für Anzeige der Bilder und schöner Ausgabe der Autorenliste

--- WebContent/WEB-INF/views/book.jsp
+++ WebContent/WEB-INF/views/book.jsp
@@ -3,11 +3,8 @@
 
 <section>
 	<h2><c:out value="${book.getTitle()}" /></h2>
-	<p>von
-		<c:forEach var="a" items="${book.getAuthors()}">
-			<c:out value="${a.getNameF()}" />&nbsp;<c:out value="${a.getNameL()}" />
-		</c:forEach>
-	</p>
+	<p>von <c:set var="delimiter" value="" scope="request"></c:set><c:forEach var="a" items="${book.getAuthors()}">${delimiter}<c:out value="${a.getNameF()}" />&nbsp;<c:out value="${a.getNameL()}" /><c:set var="delimiter" value=", " scope="request"></c:set></c:forEach></p>
 	<h4>Beschreibung</h4>
 	<p><c:out value="${book.getDescription()}"></c:out></p>
+	<img src="<c:url value="/resources/img/cover/${book.getIsbn()}.jpg"/>" title="<c:out value="Cover des Buchs '${book.getTitle()}'"/>">
 </section>
--- src/main/java/web/controllers/SingleBookController.java
+++ src/main/java/web/controllers/SingleBookController.java
@@ -32,6 +32,7 @@ public class SingleBookController {
 			// is found?
 			// TODO return correct book
 			m.addAttribute("book", getBookFromDatabase(isbn));
+			m.addAttribute("cover", "/resources/img/cover/" + getBookFromDatabase(isbn).getIsbn() + ".jpg");
 		}
 		return "book";
 	}
@@ -44,12 +45,13 @@ public class SingleBookController {
 				"This is a comprehensive tutorial that teaches fundamental and advanced SOA design principles, supplemented with detailed case studies and technologies used to implement SOAs in the real world. ***We'll have cover endorsements from Tom Glover, who leads IBM's Web Services Standards initiatives; Dave Keogh, Program Manager for Visual Studio Enterprise Tools at Microsoft, and Sameer Tyagi, Senior Staff Engineer, Sun Microsystems. All major software manufacturers and vendors are promoting support for SOA. As a result, every major development platform now officially supports the creation of service-oriented solutions. Parts I, II, and III cover basic and advanced SOA concepts and theory that prepare you for Parts IV and V, which provide a series of step-by-step 'how to' instructions for building an SOA. Part V further contains coverage of WS-* technologies and SOA platform support provided by J2EE and .NET.");
 		HashSet<Author> authorSet = new HashSet();
 		Author author = new Author();
-		author.setNameF("FirstName");
-		author.setNameL("LastName");
+		author.setNameF("Hans");
+		author.setNameL("Müller");
 		authorSet.add(author);
-		author.setNameF("FirstName yeah");
-		author.setNameL("LastName chabo");
-		authorSet.add(author);
+		Author author2 = new Author();
+		author2.setNameF("Petra");
+		author2.setNameL("Maier");
+		authorSet.add(author2);
 		dummyBook.setAuthors(authorSet);
 
 		return dummyBook;
