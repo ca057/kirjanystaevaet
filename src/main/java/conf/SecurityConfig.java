f1677757fd6e239dabb09818dc62b356c6a35ccb
Christian Ost <christian@christianost.de>
2015-12-25 13:58:23 +0100

    Konfiguration angepasst, statische Ressourcen benötigen keine Authent..

--- src/main/java/conf/SecurityConfig.java
+++ src/main/java/conf/SecurityConfig.java
@@ -2,6 +2,8 @@ package conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -20,6 +22,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// allow access to all requests which start with css, js or img
+		web.ignoring().antMatchers("/css/**").antMatchers("/js/**").antMatchers("/img/**");
 	}
 
 }
