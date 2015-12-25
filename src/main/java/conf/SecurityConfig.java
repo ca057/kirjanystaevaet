805db870a5ba3b53a9eab33afd81194d92213cc6
Christian Ost <christian@christianost.de>
2015-12-25 13:18:50 +0100

    Add SecurityConfig class; added loading in ShopInit

---
+++ src/main/java/conf/SecurityConfig.java
@@ -0,0 +1,25 @@
+package conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	public SecurityConfig() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SecurityConfig(boolean disableDefaults) {
+		super(disableDefaults);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	}
+
+}
