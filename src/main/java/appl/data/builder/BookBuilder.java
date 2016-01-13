ac765cce23d300655356695755bd5d6682ebc298
Sunflower90 <madeleine@rosenhagen.de>
2016-01-13 16:40:10 +0100

    BookBuilder

--- src/main/java/appl/data/builder/BookBuilder.java
+++ src/main/java/appl/data/builder/BookBuilder.java
@@ -1,5 +1,29 @@
 package appl.data.builder;
 
-public interface BookBuilder {
+import java.util.Set;
 
+import appl.data.items.Author;
+import appl.data.items.Category;
+
+public interface BookBuilder {
+	public BookBuilder setIsbn (String isbn);
+	
+	public BookBuilder setTitle (String title);
+	
+	public BookBuilder setDescription (String descriptiont);
+	
+	public BookBuilder setPrice (double price );
+	
+	public BookBuilder setPublisher (String publisher);
+	
+	public BookBuilder setPubdate (String pubdate);
+	
+	public BookBuilder setEdition (String edition);
+	
+	public BookBuilder setPages (int pages);
+	
+	public BookBuilder setAuthors (Set<Author> authors);
+	
+	public BookBuilder setCategories (Set<Category> categories);
+	
 }
