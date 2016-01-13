c2d900a0e72531234778caa3bcbb38d88669d227
HannesT117 <j.trepesch@outlook.de>
2016-01-13 16:09:10 +0100

    Commented buggy code

--- src/main/java/appl/logic/service/impl/UserServiceImpl.java
+++ src/main/java/appl/logic/service/impl/UserServiceImpl.java
@@ -2,27 +2,25 @@ package appl.logic.service.impl;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import appl.data.dao.UserDAO;
 import appl.data.enums.User;
 import appl.logic.service.UserService;
 
 public class UserServiceImpl implements UserService {
 
-	@Autowired
-	UserDAO userDao;
-
-	@Autowired
-
-	@Autowired
-	PasswordEncoder pswEncoder;
+	// @Autowired
+	// UserDAO userDao;
+	//
+	// @Autowired
+	//
+	//
+	// @Autowired
+	// PasswordEncoder pswEncoder;
 
 	@Override
 	public int registerNewUserAccount(Map<User, String> data) {
-		
-		appl.data.items.User user = new appl.data.items.User(nickname, name, surname, email, street, streetnumber, plz)
+
+		// appl.data.items.User user = new appl.data.items.User(nickname, name,
+		// surname, email, street, streetnumber, plz)
 		return 0;
 	}
 
