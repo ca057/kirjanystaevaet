4073e20885db0641d53aa262c520ce549e2c30b5
HannesT117 <j.trepesch@outlook.de>
2016-01-13 16:07:26 +0100

    Added Builder, started implement user service

---
+++ src/main/java/appl/data/builder/UserBuilder.java
@@ -0,0 +1,21 @@
+package appl.data.builder;
+
+import appl.data.items.User;
+
+public interface UserBuilder {
+
+	public User setNickname(String nickname);
+
+	public User setName(String name);
+
+	public User setSurname(String surname);
+
+	public User setMail(String mail);
+
+	public User setStreet(String street);
+
+	public User setStreeNumber(int number);
+
+	public User setPLZ(String plz);
+
+}
