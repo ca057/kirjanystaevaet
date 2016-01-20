9f721fd3e6ba8ac9a9ce5cc5f550b191457e3003
Christian Ost <christian@christianost.de>
2016-01-20 20:34:40 +0100

    Added comment

--- src/main/java/conf/SecurityConfig.java
+++ src/main/java/conf/SecurityConfig.java
@@ -2,7 +2,6 @@ package conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -22,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO ggf configure(...) { ... }
 		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin")
 				.password("password").roles("ADMIN");
 	}
