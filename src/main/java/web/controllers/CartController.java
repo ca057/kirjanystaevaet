e4aa4016fafd142a4754821b9ccc27f6f67ec994
Ludwig <l.thormann@gmx.de>
2016-01-20 16:45:37 +0100

    navigation.jsp: warenkorb-button: type changed CartController: return-String applied

--- src/main/java/web/controllers/CartController.java
+++ src/main/java/web/controllers/CartController.java
@@ -1,17 +1,29 @@
 package web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import appl.logic.service.BookService;
+
 @Controller
 public class CartController {
+
+	@Autowired
+	private BookService bookService;
+
+	public void setBookService(BookService bookService) {
+		this.bookService = bookService;
+	}
+
+	private final static String cartView = "cart";
 
 	@RequestMapping(value = "/warenkorb", method = RequestMethod.POST)
 	public String addToCart(@RequestParam(value = "isbn") String isbn) {
 		System.out.println(isbn);
-		return "cart";
+		return cartView;
 	}
 	// TODO: cart-logic erstellen. Nutzerspezifika implementieren, Methode:
 	// showCart: holt sich User aus Datenbank, Items im Cart und zeigt sie an
