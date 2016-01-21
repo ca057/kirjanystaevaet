f821a801432498be4b491118a91cffe9f8ebffe7
Christian Ost <christian@christianost.de>
2016-01-21 23:15:25 +0100

    Implemented first version of controller for handling registration

--- src/main/java/web/controllers/RegisterController.java
+++ src/main/java/web/controllers/RegisterController.java
@@ -1,6 +1,11 @@
 package web.controllers;
 
+import java.io.Serializable;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +13,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(path = "/registrierung")
 public class RegisterController {
 
-	public static class USER {
-		// TODO use inside class?
+	static class User implements Serializable {
+		// TODO Serializable needed?
+		private static final long serialVersionUID = 1L;
+		private String name;
+		private String surname;
+		private String email;
+		private String password;
+
+		protected String getName() {
+			return name;
+		}
+
+		protected void setName(String name) {
+			this.name = name;
+		}
+
+		protected String getSurname() {
+			return surname;
+		}
+
+		protected void setSurname(String surname) {
+			this.surname = surname;
+		}
+
+		protected String getEmail() {
+			return email;
+		}
+
+		protected void setEmail(String email) {
+			this.email = email;
+		}
+
+		protected String getPassword() {
+			return password;
+		}
+
+		protected void setPassword(String password) {
+			this.password = password;
+		}
+
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
@@ -17,10 +60,11 @@ public class RegisterController {
 		return "register";
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
-	public String registerPost() {
+	@RequestMapping(method = RequestMethod.POST, headers = { "Content-type=application/json" })
+	public ResponseEntity<String> registerPost(@RequestBody User user) {
 		// TODO implement AJAX logic here
-		return "register";
+		System.out.println("I got data!" + user.toString());
+		return new ResponseEntity<String>(user.toString(), HttpStatus.OK);
 	}
 
 }
