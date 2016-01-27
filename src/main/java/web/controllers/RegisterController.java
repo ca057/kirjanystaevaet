85bb73ef02386234c0e97c31ba1d0c53a1d667b6
Christian Ost <christian@christianost.de>
2016-01-27 16:31:22 +0100

    Deleted UserRegisterWrapper, moved it to RegisterController

--- src/main/java/web/controllers/RegisterController.java
+++ src/main/java/web/controllers/RegisterController.java
@@ -15,13 +15,83 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import appl.data.enums.UserRoles;
 import appl.data.enums.Userfields;
 import appl.data.items.User;
-import appl.data.items.UserRegisterWrapper;
 import appl.logic.service.UserService;
 import exceptions.data.PrimaryKeyViolation;
 
 @Controller
 @RequestMapping(path = "/registrierung")
 public class RegisterController {
+
+	static class UserRegisterWrapper {
+
+		private String name;
+		private String surname;
+		private String email;
+		private String password;
+		private String street;
+		private String streetnumber;
+		private String plz;
+
+		public UserRegisterWrapper() {
+
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getSurname() {
+			return surname;
+		}
+
+		public void setSurname(String surname) {
+			this.surname = surname;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getStreet() {
+			return street;
+		}
+
+		public void setStreet(String street) {
+			this.street = street;
+		}
+
+		public String getStreetnumber() {
+			return streetnumber;
+		}
+
+		public void setStreetnumber(String streetnumber) {
+			this.streetnumber = streetnumber;
+		}
+
+		public String getPlz() {
+			return plz;
+		}
+
+		public void setPlz(String plz) {
+			this.plz = plz;
+		}
+	}
 
 	@Autowired
 	private UserService userService;
