dd7ff290dc0b4e292dcd80c46b78095779e1157e
Christian Ost <christian@christianost.de>
2016-01-27 16:15:25 +0100

    Added Beans to RegisterController

--- src/main/java/web/controllers/RegisterController.java
+++ src/main/java/web/controllers/RegisterController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import appl.data.enums.UserRoles;
 import appl.data.enums.Userfields;
+import appl.data.items.User;
 import appl.data.items.UserRegisterWrapper;
 import appl.logic.service.UserService;
 import exceptions.data.PrimaryKeyViolation;
@@ -24,8 +26,15 @@ public class RegisterController {
 	@Autowired
 	private UserService userService;
 
-	public void setService(UserService userService) {
+	@Autowired
+	private DaoAuthenticationProvider authProvider;
+
+	public void setUserService(UserService userService) {
 		this.userService = userService;
+	}
+
+	public void setAuthProvider(DaoAuthenticationProvider authProvider) {
+		this.authProvider = authProvider;
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
@@ -34,7 +43,7 @@ public class RegisterController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public ResponseEntity<UserRegisterWrapper> add(@RequestBody UserRegisterWrapper req) {
+	public ResponseEntity<UserRegisterWrapper> add(@RequestBody final UserRegisterWrapper req) {
 		Map<Userfields, String> userMap = new HashMap<Userfields, String>();
 		userMap.put(Userfields.email, req.getEmail());
 		userMap.put(Userfields.name, req.getName());
@@ -45,22 +54,18 @@ public class RegisterController {
 		userMap.put(Userfields.street, req.getStreet());
 		userMap.put(Userfields.streetnumber, req.getStreetnumber());
 
-		// TODO don´t handle the success in this way, work with exceptions
-		int returnedID = 0;
-		try {
-			returnedID = userService.registerNewUserAccount(userMap);
-		} catch (PrimaryKeyViolation e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		UserRegisterWrapper returnWrapper = req;
 		req.setPassword("");
-		System.err.println("return-value: " + returnedID);
 
-		if (returnedID >= 0) {
-			// TODO try to redirect the user
+		try {
+			int id = userService.registerNewUserAccount(userMap);
+			User user = userService.findByID(id);
+			// authProvider.authenticate(new
+			// UsernamePasswordAuthenticationToken(user.getEmail(),
+			// user.getPassword()));
+
 			return new ResponseEntity<UserRegisterWrapper>(returnWrapper, HttpStatus.OK);
-		} else {
+		} catch (PrimaryKeyViolation e) {
 			return new ResponseEntity<UserRegisterWrapper>(returnWrapper, HttpStatus.UNPROCESSABLE_ENTITY);
 		}
 	}
