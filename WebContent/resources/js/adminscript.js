a5e3c0a5098e38e6c040f6b15483ac592724394c
ca057 <christian@christianost.de>
2016-02-23 22:48:09 +0100

    Remove UserRegisterWrapper from controller, add field for role

--- WebContent/resources/js/adminscript.js
+++ WebContent/resources/js/adminscript.js
@@ -1,7 +1,45 @@
-console.log("Hello Admin, you are a genius!");
+console.log('¯\\_(ツ)_/¯');
+
+const request = function(url) {
+	const ajax = function (type, data) {
+		return $.ajax({
+			url: url,
+			type: type,
+			data: JSON.stringify(data),
+			dataType: 'json',
+			contentType: 'application/json',
+		    processData: false,
+			headers: { 
+		        'Accept': 'application/json',
+		        'Content-Type': 'application/json' 
+		    },
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("X-CSRF-TOKEN", $('[name=_csrf]').val());
+			}
+		});
+	};
+	
+	return {
+		POST: (data) => ajax("POST", data),
+		GET: () => ajax('GET'),
+		DELETE: (data) => ajax('DELETE', data),
+	};
+}
 
 
+const handle = function() {
+	const user = function () {
+		// implement the user management here
+		const inputs = ["name", "surname", "street", "streetnumber", "plz", "email", "password"];
+		
+		
+	};
+	
+	return {
+		userManagement: () => user(),
+	};
+};
 
 $(document).ready(function () {
-	// do all tha sh1t
+	handle().userManagement();
 });
--- src/main/java/web/controllers/RegisterController.java
+++ src/main/java/web/controllers/RegisterController.java
@@ -17,81 +17,11 @@ import appl.data.enums.Userfields;
 import appl.data.items.User;
 import appl.logic.service.UserService;
 import exceptions.data.PrimaryKeyViolation;
+import web.jsonwrappers.UserRegisterWrapper;
 
 @Controller
 @RequestMapping(path = "/registrierung")
 public class RegisterController {
-
-	static class UserRegisterWrapper {
-
-		private String name;
-		private String surname;
-		private String email;
-		private String password;
-		private String street;
-		private String streetnumber;
-		private String plz;
-
-		public UserRegisterWrapper() {
-
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getSurname() {
-			return surname;
-		}
-
-		public void setSurname(String surname) {
-			this.surname = surname;
-		}
-
-		public String getEmail() {
-			return email;
-		}
-
-		public void setEmail(String email) {
-			this.email = email;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
-
-		public String getStreet() {
-			return street;
-		}
-
-		public void setStreet(String street) {
-			this.street = street;
-		}
-
-		public String getStreetnumber() {
-			return streetnumber;
-		}
-
-		public void setStreetnumber(String streetnumber) {
-			this.streetnumber = streetnumber;
-		}
-
-		public String getPlz() {
-			return plz;
-		}
-
-		public void setPlz(String plz) {
-			this.plz = plz;
-		}
-	}
 
 	@Autowired
 	private UserService userService;
