ce64345165d0aabe86301a464003af5e4a6b5e07
ca057 <christian@christianost.de>
2016-03-01 13:45:36 +0100

    Added some more simple staistics

--- src/main/java/conf/RootConfig.java
+++ src/main/java/conf/RootConfig.java
@@ -96,9 +96,7 @@ public class RootConfig {
 		// FIXME Johannes, you know what to do!
 		prop.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
 		// TODO Autoserver (automatic mixed mode)
-		prop.setProperty("hibernate.connection.url", "jdbc:h2:./database/kirjanystaevaet");
-		// prop.setProperty("hibernate.connection.url",
-		// "jdbc:h2:./database/kirjanystaevaet;AUTO_SERVER=TRUE");
+		prop.setProperty("hibernate.connection.url", "jdbc:h2:./database/kirjanystaevaet;AUTO_SERVER=TRUE");
 		// prop.setProperty("hibernate.c3p0.idle_test_period", "30");
 		prop.setProperty("hibernate.c3p0.testConnectionOnCheckout", "true");
 		prop.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
