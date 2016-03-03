7a43ed322e09109d88aa6a133cfe6794dc92b490
ca057 <christian@christianost.de>
2016-03-03 14:58:27 +0100

    Added JavaDoc

--- src/main/java/web/controllers/ContactController.java
+++ src/main/java/web/controllers/ContactController.java
@@ -5,13 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Controller for the route to home.
+ * Controller for the route to contact and imprint.
+ * 
+ * @author Christian
+ * @author Ludwig
+ * 
  */
 @Controller
 public class ContactController {
 
+	/**
+	 * If the user requests the contact and imprint route, this method will
+	 * return the view responsible for displaying this page.
+	 * 
+	 * @return the name of the view
+	 */
 	@RequestMapping(value = "/kontakt", method = RequestMethod.GET)
-	public String contactGet() {
+	public String getContact() {
 		return "contact";
 	}
 }
