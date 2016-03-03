ce9536e0c0c9a1a86fbbc0418556b3fd4175473f
ca057 <christian@christianost.de>
2016-03-03 12:01:25 +0100

    Improve error handling when displaying a single book.

--- src/main/java/web/controllers/SingleBookController.java
+++ src/main/java/web/controllers/SingleBookController.java
@@ -11,26 +11,50 @@ import appl.data.items.Book;
 import appl.logic.service.BookService;
 import exceptions.data.DatabaseException;
 
+/**
+ * Controller used for displaying a single book by passing a ISBN number as path
+ * variable.
+ * 
+ * @author Christian
+ * @author Ludwig
+ *
+ */
 @Controller
-@RequestMapping(value = "/buch/{isbn}")
 public class SingleBookController {
 
-	@Autowired
 	private BookService bookService;
 
+	@Autowired
 	public void setService(BookService bookService) {
 		this.bookService = bookService;
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String getBook(@PathVariable("isbn") String isbn, Model m) {
+	/**
+	 * The URL should always contain an ISBN-number, so a book can be displayed.
+	 * If no ISBN is passed, this is treated as an error, a error message is
+	 * passed to the model for informing the user.
+	 * 
+	 * @param m
+	 *            the model
+	 * @return the name of the view associated with displaying a single book
+	 */
+	@RequestMapping(value = "/buch", method = RequestMethod.GET)
+	public String getBook(Model m) {
+		m.addAttribute("error", "Die URL enthält keine ISBN, daher kann hier kein Buch angezeigt werden. "
+				+ "Möglicherweise ist der Ursprungslink fehlerhaft.");
+		return "book";
+	}
+
+	@RequestMapping(value = "/buch/{isbn}", method = RequestMethod.GET)
+	public String getBookByIsbn(@PathVariable("isbn") String isbn, Model m) {
 		if (isbn != null && !isbn.isEmpty()) {
 			try {
 				Book book = bookService.getBookByIsbn(isbn);
 				m.addAttribute("book", book);
 				m.addAttribute("authors", bookService.getAuthorByIsbn(book.getIsbn()));
 			} catch (DatabaseException e) {
-				return "book?error";
+				m.addAttribute("error",
+						"Ein Fehler ist aufgetreten. Versuchen Sie es zu einem späteren Zeitpunkt noch einmal.");
 			}
 		}
 		return "book";
