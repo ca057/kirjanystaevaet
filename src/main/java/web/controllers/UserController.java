67874185e6f6da92c0b9173abb031f6fbcd4994a
ca057 <christian@christianost.de>
2016-03-06 22:28:07 +0100

    Improve user page by displaying orders

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -1,8 +1,12 @@
 package web.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import appl.data.items.User;
 
 /**
  * Controller responsible for displaying the page of a single user.
@@ -15,13 +19,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/meinkonto")
 public class UserController {
 
+	private User getUser() {
+		return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	}
+
 	/**
 	 * Handles a GET request for rendering the page of a single user.
 	 * 
 	 * @return the name of the view
 	 */
 	@RequestMapping(method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
-	public String userGet() {
+	public String userGet(Model m) {
+		if (getUser() != null) {
+			m.addAttribute("lastOrders", getUser().getOrders());
+		}
 		return "user";
 	}
 }
