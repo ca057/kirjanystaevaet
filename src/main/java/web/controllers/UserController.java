49a14ea14f3c1f4a6c65e83912e7708826212c7a
ca057 <christian@christianost.de>
2016-03-07 22:24:55 +0100

    Orders are now displayed to user

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -1,7 +1,5 @@
 package web.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import appl.data.items.User;
+import appl.logic.service.OrderService;
 import appl.logic.service.UserService;
 import exceptions.controller.ControllerOvertaxedException;
 import exceptions.data.DatabaseException;
@@ -32,9 +31,15 @@ public class UserController {
 		this.userService = userService;
 	}
 
-	private Optional<User> getUser() throws DatabaseException {
-		return userService
-				.findByID(((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId());
+	@Autowired
+	private OrderService orderService;
+
+	public void setOrderService(OrderService orderService) {
+		this.orderService = orderService;
+	}
+
+	private int getUserId() {
+		return ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId();
 	}
 
 	/**
@@ -47,9 +52,7 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
 	public String userGet(Model m) throws ControllerOvertaxedException {
 		try {
-			User user = getUser().orElseThrow(() -> new ControllerOvertaxedException(
-					"A controller is called by an unidentifyable user. This should not be happen and seems to be a major error which can not be handled by the controller."));
-			m.addAttribute("lastOrders", user.getOrders());
+			m.addAttribute("lastOrders", orderService.getOrdersByUserid(getUserId()));
 		} catch (DatabaseException e) {
 			throw new ControllerOvertaxedException(
 					"An unknown error occured which should not happen. This can not be handled by the controller.");
