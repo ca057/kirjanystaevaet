8de57ff8e902eea00b4530863142dd8cbf1f3c0b
ca057 <christian@christianost.de>
2016-03-08 20:27:08 +0100

    Add specific method for handling 404

--- src/main/java/exceptions/web/GlobalDefaultExceptionHandler.java
+++ src/main/java/exceptions/web/GlobalDefaultExceptionHandler.java
@@ -7,13 +7,24 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
 
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ModelAndView handleHandlerNotFound(HttpServletRequest request, Exception e) {
+		ModelAndView mav = new ModelAndView("");
+		mav.addObject("exception", e);
+		mav.addObject("url", request.getRequestURL());
+		mav.setViewName("error/404");
+		return mav;
+	}
+
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(Exception.class)
-	public ModelAndView handleError404(HttpServletRequest request, Exception e) {
+	public ModelAndView defaultHandler(HttpServletRequest request, Exception e) {
 		ModelAndView mav = new ModelAndView("");
 		mav.addObject("exception", e);
 		mav.addObject("url", request.getRequestURL());
