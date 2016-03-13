45220080730cc10c628cf8b0e08bdd764b867694
ca057 <christian@christianost.de>
2016-03-14 00:26:18 +0100

    Remove unused methods

--- src/main/java/appl/data/dao/impl/BookDAOImpl.java
+++ src/main/java/appl/data/dao/impl/BookDAOImpl.java
@@ -11,7 +11,6 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.CriteriaSpecification;
-import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -212,31 +211,4 @@ public class BookDAOImpl implements BookDAO {
 		getSession().update(bookBuilder.createBook());
 		// getSession().update(book);
 	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Book> getMostVisitedBooks(int range) {
-		if (range < 0) {
-			throw new IllegalArgumentException("The passed range must be greater than 0.");
-		}
-		return setupAndGetCriteria().add(Restrictions.gt("visitCount", -1)).addOrder(Order.desc("visitCount"))
-				.setMaxResults(range).list();
-
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Book> getLeastVisitedBooks(int range) {
-		if (range < 0) {
-			throw new IllegalArgumentException("The passed range must be greater than 0.");
-		}
-		// System.out.println("Range: " + range);
-		// List result =
-		// setupAndGetCriteria().addOrder(Order.desc("visitCount")).setMaxResults(range).list();
-		// System.out.println("Least Visited: " + result);
-		// return result;
-		return setupAndGetCriteria().add(Restrictions.gt("visitCount", -1)).addOrder(Order.asc("visitCount"))
-				.setMaxResults(range).list();
-	}
-
 }
